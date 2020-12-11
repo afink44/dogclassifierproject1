3af185a3af60fb9c818f2ce153f96d9fd188b068
Andrew Fink <andrew.r.fink@gmail.com>
2020-12-10 21:43:55 -0500

    Add files via upload

--- check_images.py
+++ check_images.py
@@ -41,7 +41,6 @@ from print_results import print_results
 def main():
     # TODO 0: Measures total program runtime by collecting start time
     start_time = time()
-    sleep(1)
 
     
     # TODO 1: Define get_input_args function within the file get_input_args.py
